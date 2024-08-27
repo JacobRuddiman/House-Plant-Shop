8a012041a89117db4fe11400892a9f9c9cead320
Jacob Ruddiman <jacobruddiman@gmail.com>
2024-08-27 20:43:28 +0100

    Yep still going

--- src/components/filterBar.tsx
+++ src/components/filterBar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect, useCallback } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { getGenuses } from '@/server/plants';
