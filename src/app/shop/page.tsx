368356d584b94a181c7951d3f64cbdece30ce3ef
Jacob Ruddiman <jacobruddiman@gmail.com>
2024-08-27 20:41:34 +0100

    Still making first deploy work

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -4,12 +4,15 @@ import React from 'react';
 import FilterBar from '@/components/filterBar';
 import SortBar from '@/components/sortBar';
 import PlantGrid from '@/components/plantGrid';
+import { Suspense } from 'react';
 
 const ShopPage: React.FC = () => {
   return (
     <div className="container flex">
       <div className="w-1/6 mr-4 bg-secondary h-screen">
-        <FilterBar />
+        <Suspense>
+          <FilterBar />
+        </Suspense>
       </div>
       <div className="w-full">
         <SortBar />
