0f53fe9532e83f3c187df79e62c2db3046ccda53
Jacob Ruddiman <jacobruddiman@gmail.com>
2024-08-27 20:45:05 +0100

    You're damn right I am

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,22 +1,25 @@
+// /app/shop/page.tsx
+
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import FilterBar from '@/components/filterBar';
 import SortBar from '@/components/sortBar';
 import PlantGrid from '@/components/plantGrid';
-import { Suspense } from 'react';
 
 const ShopPage: React.FC = () => {
   return (
     <div className="container flex">
       <div className="w-1/6 mr-4 bg-secondary h-screen">
-        <Suspense>
+        <Suspense fallback={<div>Loading filters...</div>}>
           <FilterBar />
         </Suspense>
       </div>
       <div className="w-full">
-        <SortBar />
-        <PlantGrid />
+        <Suspense fallback={<div>Loading sorting and plant data...</div>}>
+          <SortBar />
+          <PlantGrid />
+        </Suspense>
       </div>
     </div>
   );
